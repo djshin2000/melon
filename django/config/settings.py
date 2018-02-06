cfab700c8c202f8df8d85fb9a98fc7d3f40811bc
Shin Dongjin <djshin2000@gmail.com>
2018-02-06 16:49:56 +0900

    artist 모델 생성, requirements.txt 파일 생성

--- django/config/settings.py
+++ django/config/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_extensions',
+
     'album.apps.AlbumConfig',
     'artist.apps.ArtistConfig',
     'song.apps.SongConfig',
+
 ]
 
 MIDDLEWARE = [
