41aacbd9dd7f3fce76a2cde54911a0c0129cfd0f
Shin Dongjin <djshin2000@gmail.com>
2018-02-08 12:49:25 +0900

    album_list, song_list views, urls, template 작성

--- django/album/views.py
+++ django/album/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Album
 
-# Create your views here.
+
+def album_list(request):
+    context = {
+        'album_list': Album.objects.all(),
+    }
+    return render(request, 'album/album_list.html', context)
--- django/song/views.py
+++ django/song/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Song
 
-# Create your views here.
+
+def song_list(request):
+    context = {
+        'song_list': Song.objects.all()
+    }
+    return render(request, 'song/song_list.html', context)
