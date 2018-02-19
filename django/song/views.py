56da7bb1dea10df09e9b332fe6843743b277ae70
Shin Dongjin <djshin2000@gmail.com>
2018-02-19 10:57:38 +0900

    song_search url, view, template 연결

--- django/song/views.py
+++ django/song/views.py
@@ -7,3 +7,8 @@ def song_list(request):
         'song_list': Song.objects.all()
     }
     return render(request, 'song/song_list.html', context)
+
+
+def song_search(request):
+
+    return render(request, 'song/song_search.html')
