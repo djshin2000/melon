9854f82108cfc024fc13ea2169616aabd85f6e31
Shin Dongjin <djshin2000@gmail.com>
2018-02-22 14:09:19 +0900

    index.html 생성 및 각App의 url 파일 수정

--- app/config/urls.py
+++ app/config/urls.py
@@ -17,9 +17,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('artist/', include('artist.urls')),
     path('song/', include('song.urls')),
