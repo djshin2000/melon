3e65dedfd0a824f6301a3d99c8847425ee0d6fe8
Shin Dongjin <djshin2000@gmail.com>
2018-02-23 12:38:06 +0900

    파일의 중복저장 방지

--- app/album/models.py
+++ app/album/models.py
@@ -28,6 +28,8 @@ class AlbumManager(models.Manager):
             album_id=album_id,
             ext=get_buffer_ext(temp_file)
         )
+        if album.img_cover:
+            album.img_cover.delete()
         album.img_cover.save(file_name, File(temp_file))
         return album, album_created
 
