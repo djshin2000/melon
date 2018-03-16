11d6a02a26cbe059f615106c682ef5d8946fd7cc
Shin Dongjin <djshin2000@gmail.com>
2018-03-16 11:51:17 +0900

    Artist.to_json()에서 동적으로 Serialize하도록 도와주는 convert_obj() 함수 구현

--- app/artist/apis/artist/list.py
+++ app/artist/apis/artist/list.py
@@ -12,16 +12,27 @@ __all__ = (
 def artist_list(request):
     artists = Artist.objects.all()
 
-    # artist_data_list = []
-    # for artist in artists:
-    #     artist_data = {
-    #         'melon_id': artist.melon_id,
-    #         'name': artist.name,
-    #     }
-    #     artist_data_list.append(artist_data)
+    # data = {
+    #     'artists': [
+    #         {
+    #             'melon_id': artist.melon_id,
+    #             'name': artist.name,
+    #             'img_profile': artist.img_profile.url if artist.img_profile else None,
+    #         }
+    #         for artist in artists],
+    # }
+
+    # data = {
+    #     'artists': [artist.to_json() for artist in artists],
+    # }
+
+    # data = {
+    #     'artists': list(artists.values()),
+    # }
 
     data = {
-        'artists': [{'melon_id': artist.melon_id, 'name': artist.name} for artist in artists],
+        'artists': [artist.to_json() for artist in artists],
     }
+
     # return HttpResponse(json.dumps(data), content_type='application/json')
     return JsonResponse(data)
